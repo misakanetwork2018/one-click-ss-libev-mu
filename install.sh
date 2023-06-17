97023a6b999de954fede9bc98ad5503e9c643fdd
misakanetwork2018 <misakanetwork2018@gmail.com>
2023-06-18 01:04:13 +0800

    fix caddy

--- install.sh
+++ install.sh
@@ -80,11 +80,13 @@ mbedtls-devel \
 libev-devel \
 c-ares-devel
 	elif [ "$1" == "Debian" ] || [ "$1" == "Raspbian" ] || [ "$1" == "Ubuntu" ];then
-	    if $install_caddy; then
-	        echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
-            | tee -a /etc/apt/sources.list.d/caddy-fury.list
-	    fi
 		$PM update
+	    if $install_caddy; then
+	        apt install -y debian-keyring debian-archive-keyring apt-transport-https
+            curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+            curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+		    $PM update
+	    fi
 		$PM -y install wget jq curl
 		$PM -y install build-essential
 		$PM -y install libpcre3-dev \
